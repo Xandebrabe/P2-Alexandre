51570a0162de62a5f7a622ac585dac1e7e3f4474
Alexandre Fonseca <80794067+Xandebrabe@users.noreply.github.com>
2023-12-15 14:34:26 -0300

    att

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # 20232BSET03P2
 Inteli - Engenharia de Software | Avaliação 2023-2B P2
+
+## Vulnerabilidades Identificadas e Medidas Adotadas:
+### SQL Injection:
+#### Vulnerabilidade 
+
+O código provido utilizava interpolação de strings diretamente em comandos SQL, o que poderia permitir injeção de SQL.
+
+#### Tratamento aplicado:
+
+ Modificação das consultas SQL para usar parâmetros preparados, evitando assim a injeção de SQL.
+
+### Validação de Entrada Insuficiente:
+#### Vulnerabilidade:
+
+Não havia validação suficiente para os parâmetros de entrada, como `animalType` e `id`.
+
+#### Tratamento Aplicado:
+
+Adição de validação para garantir que os parâmetros de entrada sejam adequados antes de executar operações no banco de dados.
+
+### Tratamento Inadequado de Erros:
+#### Vulnerabilidade:
+
+O tratamento de erros original era limitado e poderia expor detalhes de implementação ao cliente.
+
+#### Tratamento aplicado:
+
+Refatoração do tratamento de erros para fornecer mensagens padrões, sem vazar detalhes sensíveis da implementação.
+
--- index.js
+++ index.js
@@ -15,9 +15,10 @@ db.serialize(() => {
   db.run("CREATE TABLE dogs (id INT, name TEXT, votes INT)");
 });
 
+
 app.post('/cats', (req, res) => {
   const name = req.body.name;
-  db.run(`INSERT INTO cats (name, votes) VALUES ('${name}', 0)`, function(err) {
+  db.run('INSERT INTO cats (name, votes) VALUES (?, 0)', [name], function(err) {
     if (err) {
       res.status(500).send("Erro ao inserir no banco de dados");
     } else {
@@ -27,14 +28,46 @@ app.post('/cats', (req, res) => {
 });
 
 app.post('/dogs', (req, res) => {
-  
+  const name = req.body.name;
+
+  if (!name) {
+    return res.status(400).send("O nome do cachorro é obrigatório.");
+  }
+
+  db.run('INSERT INTO dogs (name, votes) VALUES (?, 0)', [name], function(err) {
+    if (err) {
+      return res.status(500).send("Erro ao inserir no banco de dados");
+    } else {
+      return res.status(201).json({ id: this.lastID, name, votes: 0 });
+    }
+  });
 });
 
+
 app.post('/vote/:animalType/:id', (req, res) => {
- 
-  db.run(`UPDATE ${animalType} SET votes = votes + 1 WHERE id = ${id}`);
-  res.status(200).send("Voto computado");
+  const { animalType, id } = req.params;
+
+  const checkQuery = `SELECT id FROM ${animalType} WHERE id = ?`;
+  db.get(checkQuery, [id], (err, row) => {
+    if (err) {
+      return res.status(500).send("Erro ao verificar o banco de dados");
+    }
+
+    if (!row) {
+      return res.status(404).send("Registro não encontrado");
+    }
+
+    const updateQuery = `UPDATE ${animalType} SET votes = votes + 1 WHERE id = ?`;
+    db.run(updateQuery, [id], function(updateErr) {
+      if (updateErr) {
+        return res.status(500).send("Erro ao atualizar o banco de dados");
+      }
+
+      return res.status(200).send("Voto computado");
+    });
+  });
 });
+
 
 app.get('/cats', (req, res) => {
   db.all("SELECT * FROM cats", [], (err, rows) => {
@@ -47,7 +80,13 @@ app.get('/cats', (req, res) => {
 });
 
 app.get('/dogs', (req, res) => {
-  
+  db.all("SELECT * FROM dogs", [], (err, rows) => {
+    if (err) {
+      res.status(500).send("Erro ao consultar o banco de dados");
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 app.use((err, req, res, next) => {
